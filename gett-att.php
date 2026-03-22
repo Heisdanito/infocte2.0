067629eba462d719cef3f0e8f9d05b3e6b39605d
heisdanito <heisdanito@gmail.com>
2026-03-22 03:32:32 +0000

    Update gett-att.php

--- gett-att.php
+++ gett-att.php
@@ -3,7 +3,8 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if ($_SERVER["REQUEST_METHOD"] !== "OPTIONS") { http_response_code(200); exit(); }
 
 require_once __DIR__ . '/connection.php';
